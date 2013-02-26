943b5c04c16c28790d64b7fb9fb54ede1d0ec2b7
David Carlton <davidacarlton@gmail.com>
2013-02-26 13:56:17 -0800

    Update long now encoding/VowelsAndConsonantsJava/vowcon.java

--- long now encoding/VowelsAndConsonantsJava/vowcon.java
+++ long now encoding/VowelsAndConsonantsJava/vowcon.java
@@ -11,21 +11,27 @@ if(asc==97||asc==101||asc==105||asc==111||asc==117) {
 
 vow++; vowL++;
 } else if(asc==65||asc==69||asc==73||asc==79||asc==85) {
+
 vow++; vowU++;
 } else if((asc>97&&asc<101)||(asc>101&&asc<105)||(asc>105&&asc<111)||(asc>111&&asc<117)||(asc>117&&asc<123)) {
+
 cons++; consL++;
 } else if((asc>65&&asc<69)||(asc>69&&asc<73)||(asc>73&&asc<79)||(asc>79&&asc<85)||(asc>85&&asc<91)) {
+
 cons++; consU++;
 } else if((asc>47&&asc<58)){
+
 num++;
 } else if(asc==32){
+
 sp++;
 } else {
+
 other++;
 }
 }
-System.out.println("count of upper case vowels: " + vowU); System.out.println("num of lower case vowels: " + vowL); System.out.println("all vowels: " + vow); 
-System.out.println("num of upper case consonants: " + consU); System.out.println("num of lower case consonants: " + consL); System.out.println("all consonants: " + cons); 
+System.out.println("num of lower case vowels: " + vowL); System.out.println("num of upper case vowels: " + vowU); System.out.println("all vowels: " + vow); 
+System.out.println("num of lower case consonants: " + consU); System.out.println("num of upper case consonants: " + consU); System.out.println("all consonants: " + cons); 
 System.out.println("num of number characters: " + num); System.out.println("num of spaces: " + sp); System.out.println("other characters: " + other);
 }
 } 
